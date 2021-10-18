5a27aba915b9032e839dbc4ba3e03046dc1d4104
GabrielMM182 <gmm_morais@hotmail.com>
2021-10-18 12:40:23 -0300

    pt2

---
+++ app/controllers/negociacao-controller.ts
@@ -0,0 +1,60 @@
+import { Negociacao } from '../models/negociacao.js';
+import { Negociacoes } from '../models/negociacoes.js';
+import { MensagemView } from '../views/mensagem-view.js';
+import { NegociacoesView } from '../views/negociacoes-view.js';
+import { DiasDaSemana } from '../enums/dias-da-semana.js';
+
+export class NegociacaoController {
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
+    private negociacoesView = new NegociacoesView('#negociacoesView');
+    private mensagemView = new MensagemView('#mensagemView');
+
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+        this.negociacoesView.update(this.negociacoes);
+    }
+
+    public adiciona(): void {
+        const negociacao = this.criaNegociacao();
+        // ! faz papel de else 
+        if (!this.ehDiaUtil(negociacao.data)) {
+            this.mensagemView.update('Sabado e Domingo não é disponivel')
+            return;
+        }
+            this.negociacoes.adiciona(negociacao);
+            this.limparFormulario();
+            this.atualizaView();
+
+    }
+
+    private ehDiaUtil(data: Date) {
+        // fazer com que sabado e domingo não seja disponivel para concluir 
+        return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO;
+    }
+
+    private criaNegociacao(): Negociacao {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+
+
+    private limparFormulario(): void {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+
+    private atualizaView(): void {
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com sucesso');
+    }
+}
